58ee11965c89777025ff1f9cfee9099510edde03
Kobayashi Takahiro <koba925@gmail.com>
2022-11-23 18:25:59 +0900

    ITP1_4_C

---
+++ ITP1/4_C.py
@@ -0,0 +1,11 @@
+# 4_C.py
+
+while True:
+    a, op, b = input().split()
+    if op == "?": break
+    a = int(a)
+    b = int(b)
+    if op == "+": print(a + b)
+    elif op == "-": print(a - b)
+    elif op == "*": print(a * b)
+    elif op == "/": print(a // b)
