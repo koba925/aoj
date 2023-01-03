9c9a04074eb875aa03626ff62854e8a29ff3d0c5
Kobayashi Takahiro <koba925@gmail.com>
2023-01-03 16:41:41 +0900

    ITP1_7_B refactored

--- ITP1/7_B.py
+++ ITP1/7_B.py
@@ -4,10 +4,10 @@ def solve(n, x):
     count = 0
     for i in range(1, n - 1):
         for j in range(i + 1, n):
-            for k in range(j + 1, n + 1):
-                if i + j + k == x:
-                    # print(i, j, k)
-                    count += 1
+            k = x - i - j
+            if j < k <= n:
+                # print(i, j, k)
+                count += 1
     return count
 
 while True:
