24f90b63b87af46079eddd4520423fdc32eb7fac
Kobayashi Takahiro <koba925@gmail.com>
2023-01-07 10:50:15 +0900

    ITP1_7_D refactor 3

--- ITP1/7_D.py
+++ ITP1/7_D.py
@@ -8,14 +8,15 @@ class Matrix:
         self.a = [[0 for e in range(m)] for e in range(n)]
 
     def read(self):
-        for i in range(self.row):
-            self.a[i] = [int(e) for e in input().split()]
+        self.a = [[int(e) for e in input().split()] for i in range(self.row)]
+        return self
 
     def mul(self, other):
         result = Matrix(self.row, other.col)
         for i in range(self.row):
             for j in range(other.col):
-                result.a[i][j] = sum([self.a[i][k] * other.a[k][j] for k in range(self.col)])
+                result.a[i][j] = sum([self.a[i][k] * other.a[k][j]
+                                     for k in range(self.col)])
         return result
 
     def print(self):
@@ -25,8 +26,6 @@ class Matrix:
 
 n, m, l = [int(e) for e in input().split()]
 
-a = Matrix(n, m)
-b = Matrix(m, l)
-a.read()
-b.read() 
+a = Matrix(n, m).read()
+b = Matrix(m, l).read()
 a.mul(b).print()
