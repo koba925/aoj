3e4b500544eecc8ebf828306f43d1127905f8340
Kobayashi Takahiro <koba925@gmail.com>
2023-01-09 09:48:39 +0900

    ITP1_8_D: use for-else

--- ITP1/8_D.py
+++ ITP1/8_D.py
@@ -3,4 +3,12 @@
 s = input()
 p = input()
 
-print("Yes" if p in s + s else "No") 
+for i in range(len(s)):
+    for j in range(len(p)):
+        if s[(i + j) % len(s)] != p[j]:
+            break
+    else:
+        print("Yes")
+        break
+else:
+    print("No")
